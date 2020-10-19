2ebd493081014c4516f69f93dd38fc3389fad709
Freddy <oldfredddy@gmail.com>
2020-10-19 11:15:20 +0300

    parsing, циклы, массивы

--- app/src/main/java/com/oldfredddy/kotlin_lesson_1/MainActivity.kt
+++ app/src/main/java/com/oldfredddy/kotlin_lesson_1/MainActivity.kt
@@ -2,15 +2,15 @@ package com.oldfredddy.kotlin_lesson_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var number1: Int = 6
     private var number2: Int = 6
-
-    //private var text: String = "В магазине осталось $number1 яблок"
-    private var text: String = "В <магазине> осталось 123 яблока, а может и <больше, кто знает>..."
+    private var text: String =
+        "В <магазине> осталось 123 яблока, а может и <больше, кто знает> всем привет <как> у вас дела <надеюсь>  все <хорошо>"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +26,39 @@ class MainActivity : AppCompatActivity() {
         var substring3 = text.substringAfter('>')
         var substring4 = substring3.substringAfter('<')
         var substring5 = substring4.substringBefore('>')
-        tvText.text = substring2
-        tvText2.text = substring5
+        var counter: Int = 0
+//        var startNumber = Array(3){}
+//        var startNumber: Array<Int> = arrayOf(2,6,7,8)
+//        var startNumber: Array<Int>
+//        var startNumber= IntArray(3)
+        for (n in text.indices) {
+            if (text.get(n) == '<') counter++
+            tvText.text = counter.toString()
+        }
+        var startPosition = IntArray(counter)
+        var endPosition = IntArray(counter)
+        var startPcounter: Int = 0
+        var endPcounter: Int = 0
+
+        for (n in text.indices) {
+            if (text[n] == '<') {
+                startPosition[startPcounter] = n
+                startPcounter++
+            }
+        }
+        for (n in text.indices) {
+            if (text[n] == '>') {
+                endPosition[endPcounter] = n
+                endPcounter++
+            }
+        }
+
+        var textFoundArray = Array(counter) { "" }
+
+        for (n in startPosition.indices) {
+            textFoundArray[n] = text.substring(startPosition[n] + 1 , endPosition[n])
+            Log.d("MyLog", "Text from n : " + textFoundArray[n])
+        }
 
 
     }
